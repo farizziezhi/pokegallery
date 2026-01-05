d4bfe7d4f50ce9d9c0c9c0466c6efe25bf9efb83
muh.farizzi <ezhifarizzi@gmail.com>
2026-01-05 18:55:33 +0800

    add: add detail to models

--- lib/models/pokemon.dart
+++ lib/models/pokemon.dart
@@ -2,11 +2,13 @@ class Pokemon {
   final int id;
   final String name;
   final String imageUrl;
+  final List<String> types;
 
   Pokemon({
     required this.id,
     required this.name,
     required this.imageUrl,
+    this.types = const [],
   });
 
   factory Pokemon.fromJson(Map<String, dynamic> json) {
@@ -20,6 +22,32 @@ class Pokemon {
       id: id,
       name: json['name'] as String,
       imageUrl: imageUrl,
+      types: [],
+    );
+  }
+
+  factory Pokemon.fromDetailJson(Map<String, dynamic> json) {
+    final id = json['id'] as int;
+
+    final typesData = json['types'] as List<dynamic>;
+    final types = typesData.map((t) => t['type']['name'] as String).toList();
+
+    final imageUrl = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/$id.png';
+
+    return Pokemon(
+      id: id,
+      name: json['name'] as String,
+      imageUrl: imageUrl,
+      types: types,
+    );
+  }
+
+  Pokemon copyWithTypes(List<String> newTypes) {
+    return Pokemon(
+      id: id,
+      name: name,
+      imageUrl: imageUrl,
+      types: newTypes,
     );
   }
 
@@ -27,4 +55,6 @@ class Pokemon {
     if (name.isEmpty) return name;
     return name[0].toUpperCase() + name.substring(1);
   }
+
+  String get primaryType => types.isNotEmpty ? types.first : 'normal';
 }
